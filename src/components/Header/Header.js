926bd38247b4080d547db5e2ba7d312f24f01031
juniorovi <mdtarikulislam258000@gmail.com>
2023-02-11 09:24:54 +0600

    active link showed by color and underline

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to React Routing!</h1>
+            <CustomLink to='/'>Home</CustomLink>
+            <CustomLink to='/friends'>Friends</CustomLink>
+            <CustomLink to='/about'>Abouts</CustomLink>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>The page you are locking for not Available!!</h1>
+            <h1>404</h1>
+        </div>
+    );
+};
+
+export default NotFound;
